a1de19bb8ae42de1d6d2431a6255f9832895a397
Maxwell Conrad <mvxconrad@gmail.com>
2025-11-21 12:55:04 -0500

    Switch to HashRouter for Github Pages, add .vite to .gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 // Import Bootswatch Cyborg theme
@@ -12,8 +12,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
